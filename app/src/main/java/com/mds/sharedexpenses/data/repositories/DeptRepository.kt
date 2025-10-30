915a26c781b07472550c8b6fce20ba74b754f54a
Adam Nejdl <nejdlad1@fel.cvut.cz>
2025-10-30 11:38:29 +0100

    added Dept and Expanse classes

---
+++ app/src/main/java/com/mds/sharedexpenses/data/repositories/DeptRepository.kt
@@ -0,0 +1,4 @@
+package com.mds.sharedexpenses.data.repositories
+
+
+data class Dept(val id: Int, val group: String, val user: String, val expense: List<Int>)
--- app/src/main/java/com/mds/sharedexpenses/data/repositories/ExpenseRepository.kt
+++ app/src/main/java/com/mds/sharedexpenses/data/repositories/ExpenseRepository.kt
@@ -1,6 +1,13 @@
 package com.mds.sharedexpenses.data.repositories
 
-data class Expense(val amount: Double, val description: String) // TODO: please adopt
+import androidx.annotation.Nullable
+
+data class Expense(val id: Int,
+                    val payer: String,
+                    val deptors: List<String>,
+                    val amount: Double,
+                    var name: String,
+                    var description: String)
 
 interface ExpenseRepositoryInterface {
     fun createExpense(expense: Expense)
