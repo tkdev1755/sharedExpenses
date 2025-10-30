8cfe33680268d9015ac6f6048f708e7f1d913183
= <hannes.vinzens@fau.de>
2025-10-30 11:45:20 +0100

    scaffold and action button added

--- app/src/main/java/com/mds/sharedexpenses/ui/home/HomeScreen.kt
+++ app/src/main/java/com/mds/sharedexpenses/ui/home/HomeScreen.kt
@@ -1,6 +1,8 @@
 package com.mds.sharedexpenses.ui.home
 
 import android.R.attr.contentDescription
+import android.graphics.drawable.Icon
+import androidx.collection.objectListOf
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,37 +13,50 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ChevronLeft
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Group
+import androidx.compose.material.icons.filled.GroupAdd
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.mds.sharedexpenses.ui.components.CustomActionButton
 import com.mds.sharedexpenses.ui.components.HeaderTopBar
+import com.mds.sharedexpenses.ui.navigation.Screen
 import com.mds.sharedexpenses.ui.theme.SharedExpensesTheme
 
 
 @Composable
 fun HomeScreen(
+    navController: NavController,
     viewModel: HomeViewModel = HomeViewModel()
 ) {
     Scaffold(
         topBar = {
             HeaderTopBar("<add title!>")
         },
-//        floatingActionButton = {
-//            FloatingActionButton()
-//        }
+        floatingActionButton = {
+            CustomActionButton(
+                imageVector = Icons.Filled.GroupAdd,
+                iconContentDescription = "Click to create a new Group",
+                text = "Create Group",
+                onClick = {viewModel.onButtonClicked()})
+        }
     ){ innerPadding ->
         Column(
             modifier = Modifier
@@ -52,9 +67,9 @@ fun HomeScreen(
         ) {
             Text(text = "home screen :D")
             Button(onClick = {
-                viewModel.onButtonClicked()
+                navController.navigate(Screen.GroupDetail.route)
             }) {
-                Text(text = "Click me")
+                Text(text = "Navigate to group detail")
             }
         }
     }
@@ -69,6 +84,6 @@ fun HomeScreen(
 @Composable
 fun HomeScreenPreview() {
     SharedExpensesTheme {
-        HomeScreen()
+        HomeScreen(navController = NavController(context = androidx.compose.ui.platform.LocalContext.current))
     }
 }
--- app/src/main/java/com/mds/sharedexpenses/ui/navigation/Screen.kt
+++ app/src/main/java/com/mds/sharedexpenses/ui/navigation/Screen.kt
@@ -3,7 +3,6 @@ package com.mds.sharedexpenses.ui.navigation
 //listing all screens of the app
 sealed class Screen(val route: String) {
     object Home : Screen("home")
-
-    // object GroupDetail : Screen("group_detail")
-    // object Profile : Screen("profile")
+    object GroupDetail : Screen("group_detail")
+    object Profile : Screen("profile")
 }
