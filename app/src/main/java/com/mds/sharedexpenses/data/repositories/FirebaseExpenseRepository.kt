152a0b344b487352884ab30784b67c64647b4200
Adam Nejdl <nejdlad1@fel.cvut.cz>
2025-11-11 00:17:11 +0100

    init commit

--- app/src/main/java/com/mds/sharedexpenses/data/repositories/FirebaseExpenseRepository.kt
+++ app/src/main/java/com/mds/sharedexpenses/data/repositories/FirebaseExpenseRepository.kt
@@ -1,3 +1,10 @@
 package com.mds.sharedexpenses.data.repositories
 
+import com.google.firebase.database.DatabaseReference
+import com.mds.sharedexpenses.domain.repository.FirebaseRepository
+
 // Add the functions that will communicate with the Firebase
+class FIRExpenseRepository(private val firebaseRepository : FirebaseRepository){
+    
+
+}
