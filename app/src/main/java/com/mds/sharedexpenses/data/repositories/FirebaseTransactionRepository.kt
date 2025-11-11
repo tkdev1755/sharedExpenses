6500c1199ab3e38ad90982666bbcb100b2c87d65
amauol <chau.amaury@gmail.com>
2025-11-11 09:45:15 +0100

    Recover the Transaction directory of a specific user

--- app/src/main/java/com/mds/sharedexpenses/data/repositories/FirebaseTransactionRepository.kt
+++ app/src/main/java/com/mds/sharedexpenses/data/repositories/FirebaseTransactionRepository.kt
@@ -1,3 +1,14 @@
 package com.mds.sharedexpenses.data.repositories
 
 // Add the functions that will communicate with the Firebase
+import com.google.firebase.database.DatabaseReference
+import com.mds.sharedexpenses.data.utils.DataResult
+import com.mds.sharedexpenses.domain.repository.FirebaseRepository
+import com.mds.sharedexpenses.data.models.Transaction
+
+class FirebaseTransactionRepository(private val firebaseRepository : FirebaseRepository){
+    fun getTransactionDirectory() : DatabaseReference {
+        val userDirectory : DatabaseReference = firebaseRepository.getUserDirectory()
+        return userDirectory.child("transaction")
+    }
+}
