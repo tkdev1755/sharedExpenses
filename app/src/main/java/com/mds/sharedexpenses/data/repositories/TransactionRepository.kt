bab11e2fc2fd0e359b720c327b07f75d55c5e582
amauol <chau.amaury@gmail.com>
2025-11-12 16:02:41 +0100

    Begin with the transaction methods (serialize and deserialize)

--- app/src/main/java/com/mds/sharedexpenses/data/repositories/TransactionRepository.kt
+++ app/src/main/java/com/mds/sharedexpenses/data/repositories/TransactionRepository.kt
@@ -6,6 +6,8 @@ import com.mds.sharedexpenses.data.models.Transaction
 
 
 interface TransactionRepositoryInterface {
+    fun serialize(transaction: Transaction)
+    suspend fun deserialize(data: Map<String, Any?>, groupId: String)
     fun createTransaction(transaction: Transaction)
     fun deleteTransaction(transaction: Transaction)
     fun updateTransaction(transaction: Transaction)
