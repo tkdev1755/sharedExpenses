7d5be6ebb2e0c0a6d8a97a9e09b32943c92ce66c
= <hannes.vinzens@fau.de>
2025-11-12 16:33:33 +0100

    hotfix: added correct icon library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
