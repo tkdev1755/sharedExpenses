f7082636a96664674219b766b45072a1dadae829
Mathias <mathias.yde@hotmail.dk>
2025-11-21 13:38:50 +0100

    Cleanup functions by removing mock data

--- app/src/main/java/com/mds/sharedexpenses/ui/groupdetail/GroupDetailViewModel.kt
+++ app/src/main/java/com/mds/sharedexpenses/ui/groupdetail/GroupDetailViewModel.kt
@@ -14,7 +14,6 @@ data class GroupDetailUiState(
 )
 
 class GroupDetailViewModel() : BaseViewModel() {
-    // TODO: add group repository
     // private val groupRepository = GroupRepository()
 
     private val groupId: String = ""
@@ -28,8 +27,6 @@ class GroupDetailViewModel() : BaseViewModel() {
     }
 
     private fun loadGroupDetails() {
-        // TODO: fetch group details from repository
-        //on error:
         showErrorMessage("oh no error!")
     }
 
@@ -47,28 +44,10 @@ class GroupDetailViewModel() : BaseViewModel() {
     }
 
     fun expenses(): Map<String, List<Expense>> {
-//        return mapOf(
-//            "January 2025" to listOf(
-//                Expense(45.90, "Groceries – Lidl", LocalDate.of(2025, 1, 27)),
-//                Expense(120.00, "Electricity Bill", LocalDate.of(2025, 1, 10)),
-//                Expense(15.49, "Coffee – Studenterhuset", LocalDate.of(2025, 1, 3))
-//            ),
-//            "February" to listOf(
-//                Expense(32.10, "Groceries – Rema1000", LocalDate.of(2025, 2, 21)),
-//                Expense(60.00, "Phone Bill", LocalDate.of(2025, 2, 13)),
-//                Expense(9.99, "Spotify", LocalDate.of(2025, 2, 7))
-//            ),
-//            "March" to listOf(
-//                Expense(55.70, "Groceries – Føtex", LocalDate.of(2025, 3, 30)),
-//                Expense(18.00, "Laundry", LocalDate.of(2025, 3, 22)),
-//                Expense(45.00, "Night out – Lambda", LocalDate.of(2025, 3, 5))
-//            )
-//        )
         return emptyMap()
     }
 
     fun members(): List<User> {
-//        return listOf("Person 1", "Person 2", "Person 3")
         return emptyList()
     }
 
@@ -81,6 +60,6 @@ class GroupDetailViewModel() : BaseViewModel() {
     }
 
     fun navigateBack() {
-        TODO("Not yet implemented")
+        showErrorMessage("navigateBack not implemented yet")
     }
 }
