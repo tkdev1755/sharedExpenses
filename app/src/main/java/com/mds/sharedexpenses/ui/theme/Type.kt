d4bf98d16e89fca97f0b52528f17c0cb231c12a8
= <hannes.vinzens@fau.de>
2025-11-29 11:47:20 +0100

    added new color pallate and frame for custom font implementation

--- app/src/main/java/com/mds/sharedexpenses/ui/theme/Type.kt
+++ app/src/main/java/com/mds/sharedexpenses/ui/theme/Type.kt
@@ -2,10 +2,37 @@ package com.mds.sharedexpenses.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mds.sharedexpenses.R
 
+//TODO: add correct files once they are added
+//val howeverYourFontIsNamedTaha = FontFamily(
+//    Font(R.font.tahaLight, FontWeight.Light),
+//    Font(R.font.tahaNormal, FontWeight.Normal),
+//    Font(R.font.tahaLight, FontWeight.Bold),
+//    Font(R.font.tahaNormalItalic, FontWeight.Normal, FontStyle.Italic)
+//)
+//val AppTypography = Typography(
+//    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+//    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+//    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+//    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+//    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+//    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+//    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+//    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+//    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+//    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+//    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+//    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+//    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+//    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+//    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+//)
 // Set of Material typography styles to start with
 val Typography =
     Typography(
